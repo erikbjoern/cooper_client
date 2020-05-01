700040d39cc7a0461596c4875fd4e11a02cb266a
pauline_barnades <barnades.pauline@gmail.com>
2020-05-01 14:09:48 +0200

    Rendering the message when request is made

--- src/App.jsx
+++ src/App.jsx
@@ -33,19 +33,32 @@ class App extends Component {
       }
     };
 
-  
-
-  render() {
-    const renderLogin = this.state.renderLoginForm ? (
-      <LoginForm submitFormHandler={this.onLogin}/>
-    ) : (
-      <button 
-      id="login"
-      onClick={() => this.setState({renderLoginForm: true })}
-      
-      >Login
-      </button>
-    );
+    render() {
+      const { renderLoginForm, authenticated, message } = this.state;
+      let renderLogin;
+      switch(true) {
+        case renderLoginForm && !authenticated:
+          renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+          break;
+        case !renderLoginForm && !authenticated:
+          renderLogin = (
+            <>
+              <button
+                id="login"
+                onClick={() => this.setState({ renderLoginForm: true })}
+              >
+                Login
+              </button>
+              <p id="message">{message}</p>
+            </>
+          );
+          break;
+        case authenticated:
+          renderLogin = (
+            <p id="message">Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+          );
+          break;
+      }
     return (
       <>
         <InputFields onChangeHandler={this.onChangeHandler} />
