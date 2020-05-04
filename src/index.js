313dd5322a44e923257f45950489bd1d2fa40af0
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-04 07:47:54 +0200

    putting index.html in src for production

--- src/index.js
+++ src/index.js
@@ -6,16 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 import 'semantic-ui-css/semantic.min.css'
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "https://cooper-calculator.herokuapp.com/api/v1";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
